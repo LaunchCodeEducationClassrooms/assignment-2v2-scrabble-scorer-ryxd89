6d07ed2b690ee09ec1b310dac8c546a30b8b8dfe
Da Rynoceros <ryxd89@gmail.com>
2021-02-23 18:25:02 +0000

    Added functions

--- scrabble-scorer.js
+++ scrabble-scorer.js
@@ -11,40 +11,74 @@ const oldPointStructure = {
   8: ['J', 'X'],
   10: ['Q', 'Z']
 };
+const simpleScoreStructure = {
+  1: ['A', 'B', 'C','D','E','F','G', 'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+};
+
+const vowelBonusStructure = {
+  1: ["B",'C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'],
+  3: ['A','E','I','O','U']
+}
 
 function oldScrabbleScorer(word) {
 	word = word.toUpperCase();
 	let letterPoints = "";
- 
 	for (let i = 0; i < word.length; i++) {
- 
-	  for (const pointValue in oldPointStructure) {
- 
+	  for (const pointValue in oldPointStructure
+    ) {
 		 if (oldPointStructure[pointValue].includes(word[i])) {
 			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
 		 }
- 
 	  }
 	}
 	return letterPoints;
  }
 
+ function vowelScorer(word) {
+	word = word.toUpperCase();
+	let letterPoints = "";
+	for (let i = 0; i < word.length; i++) {
+	  for (const pointValue in vowelBonusStructure) {
+		 if (vowelBonusStructure[pointValue].includes(word[i])) {
+			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+		 }
+	  }
+	}
+	return letterPoints;
+ };
+
+ function simpleScorer(word) {
+	word = word.toUpperCase();
+	let letterPoints = "";
+	for (let i = 0; i < word.length; i++) {
+	  for (const pointValue in simpleScoreStructure ) {
+		 if (simpleScoreStructure[pointValue].includes(word[i])) {
+			letterPoints += `Points for '${word[i]}': ${pointValue}\n`
+		 }
+	  }
+	}
+	return letterPoints;
+ };
+
 // your job is to finish writing these functions and variables that we've named //
 // don't change the names or your program won't work as expected. //
 
 function initialPrompt() {
-   console.log("Let's play some scrabble! Enter a word:");
+   let word = input.question("Let's play some scrabble! Enter a word: ");
+   console.log(oldScrabbleScorer(word))
 };
 
-let simpleScore;
+let simpleScore = "Simple Score"
 
-let vowelBonusScore;
+let vowelBonusScore = "Bonus Vowels"
 
-let scrabbleScore;
+let scrabbleScore = "Scrabble";
 
-const scoringAlgorithms = [];
+const scoringAlgorithms = [oldScrabbleScorer, simpleScore, vowelBonusScore];
 
-function scorerPrompt() {}
+function scorerPrompt(name, description, func) {
+  
+}
 
 function transform() {};
 
